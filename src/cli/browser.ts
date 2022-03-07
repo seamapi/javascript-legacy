53456661ef209af33799c68424461ee200a7c877
Max Isom <hi@maxisom.me>
2022-03-07 17:07:17 -0500

    Add bundle for browser

---
+++ src/cli/browser.ts
@@ -0,0 +1,5 @@
+import { getCLI } from "."
+// todo: use latest version
+import { Yargs } from "https://unpkg.com/yargs@16.0.0-alpha.3/browser.mjs"
+
+export const cli = getCLI(Yargs())
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "tsup"
+import { cache } from "esbuild-plugin-cache"
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/cli/**/*.ts"],
+  entry: [
+    "src/index.ts",
+    "src/cli/index.ts",
+    "src/cli/entry.ts",
+    "src/cli/browser.ts",
+  ],
   dts: true,
   sourcemap: true,
   format: ["cjs", "esm"],
   env: {
     NODE_ENV: "production",
   },
+  esbuildPlugins: [cache({})],
 })
