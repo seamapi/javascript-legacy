f0d1f56cbabfa57d4bdbf78d762943f9104e7d2e
Itelo Filho <itelofilho@gmail.com>
2023-04-17 20:18:06 -0300

    final try

--- tests/fixtures/workers/helpers/add-fake-minut-devices.ts
+++ tests/fixtures/workers/helpers/add-fake-minut-devices.ts
@@ -2,29 +2,9 @@ import { Axios, AxiosResponse } from "axios"
 import getDeviceType from "./get-device-type"
 import { AxiosError } from "axios"
 
-async function retryAxiosCall(
-  axios_fn: Promise<AxiosResponse<any, any>>,
-  maxRetries: number
-) {
+async function retryAxiosCall(axios: Axios, maxRetries: number) {
   let retryCount = 0
 
-  while (retryCount < maxRetries) {
-    try {
-      await axios_fn
-    } catch (error) {
-      console.log(
-        `Attempt ${retryCount + 1} failed. Error: ${
-          (error as AxiosError).response?.data.error.message
-        }`
-      )
-      retryCount++
-    }
-  }
-
-  throw new Error(`Failed load Minut scenario`)
-}
-
-const addFakeMinutDevices = async (axios: Axios) => {
   const defaultConfig = {
     sound_level_high: {
       value: 60,
@@ -41,27 +21,39 @@ const addFakeMinutDevices = async (axios: Axios) => {
     },
   }
 
-  const create_minut_devices = axios.post(
-    "/internal/scenarios/factories/load",
-    {
-      factory_name: "create_minut_devices",
-      input: {
-        devicesConfig: [
-          defaultConfig,
-          {
-            ...defaultConfig,
-            sound_level_high_quiet_hours: {
-              ...defaultConfig.sound_level_high_quiet_hours,
-              enabled: false,
+  while (retryCount < maxRetries) {
+    try {
+      await axios.post("/internal/scenarios/factories/load", {
+        factory_name: "create_minut_devices",
+        input: {
+          devicesConfig: [
+            defaultConfig,
+            {
+              ...defaultConfig,
+              sound_level_high_quiet_hours: {
+                ...defaultConfig.sound_level_high_quiet_hours,
+                enabled: false,
+              },
             },
-          },
-        ],
-      },
-      sync: true,
+          ],
+        },
+        sync: true,
+      })
+    } catch (error) {
+      console.log(
+        `Attempt ${retryCount + 1} failed. Error: ${
+          (error as AxiosError).response?.data.error.message
+        }`
+      )
+      retryCount++
     }
-  )
+  }
 
-  await retryAxiosCall(create_minut_devices, 5)
+  throw new Error(`Failed load Minut scenario`)
+}
+
+const addFakeMinutDevices = async (axios: Axios) => {
+  await retryAxiosCall(axios, 5)
 
   const devices = await getDeviceType(axios, "minut_sensor")
   const [device_with_quiet_hours, device_without_quiet_hours] = devices
