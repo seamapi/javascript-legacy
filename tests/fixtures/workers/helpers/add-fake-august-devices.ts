19e0a1a48604bd1ef46e221d022fa8dd0b1ad189
Max Isom <hi@maxisom.me>
2023-05-15 11:25:11 -0700

    Fix tests?

--- tests/fixtures/workers/helpers/add-fake-august-devices.ts
+++ tests/fixtures/workers/helpers/add-fake-august-devices.ts
@@ -13,11 +13,7 @@ const addFakeAugustDevices = async (axios: Axios) => {
   const [firstDevice] = await getDeviceType(axios, "august_lock")
 
   const {
-    data: {
-      action_attempt: {
-        result: { access_code },
-      },
-    },
+    data: { access_code },
   } = await axios.post("/access_codes/create", {
     device_id: firstDevice.device_id,
     name: "Sample Code",
