7f1fcf78471283333480ed0c0f9df27d9cc2351c
Max Isom <hi@maxisom.me>
2023-06-20 08:35:33 -0700

    Flatten webhook types

--- src/types/webhook-events.ts
+++ src/types/webhook-events.ts
@@ -3,8 +3,7 @@ import { SeamEvent } from "seamapi-types"
 type Distribute<EventType> = EventType extends SeamEvent["event_type"]
   ? {
       event_type: EventType
-      data: Extract<SeamEvent, { event_type: EventType }>["payload"]
-    }
+    } & Extract<SeamEvent, { event_type: EventType }>["payload"]
   : never
 
 export type SeamWebhookEvent = Distribute<SeamEvent["event_type"]>
